bafc44647010d2cdfd79d64c19c9cfd1b808b90b
BalkoBalkho <71823820+BalkoBalkho@users.noreply.github.com>
2021-11-15 13:46:23 +0500

    woops

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ var log = []
 
 if (!process.env.disableserver) {
 
-    http.createServer((request, response) => { 
+   const server = http.createServer((request, response) => { 
         response.writeHead(200, {
             'Content-Type': 'text/plain'
         });
         response.write(log.join('\n'))
         response.end()
     })
+
+    server.listen(80)
 
 }
 
